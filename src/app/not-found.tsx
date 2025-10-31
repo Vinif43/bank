7177c6a6abd53b27153f548436d9fd30a26243e3
AdrianeSS <adriane_silvasanches@hotmail.com>
2025-10-30 22:00:24 -0300

    feat: implementando responsividade ne tela not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,7 +7,7 @@ import { Landmark } from 'lucide-react'
 export default function NotFound() {
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="bg-primary text-primary-foreground px-8 py-4 flex items-center justify-between">
+      <header className="bg-primary text-primary-foreground px-8 py-4 flex items-center justify-center md:justify-start">
         <div className="flex items-center">
           <Landmark className="h-7 w-7 text-[#ffd21f]" />
           <img
@@ -20,7 +20,7 @@ export default function NotFound() {
 
       <main className="flex flex-1 flex-col items-center justify-center bg-[#540057] bg-[linear-gradient(180deg,_rgba(84,_0,_87,_1)_0%,_rgba(255,_255,_205,_1)_70%,_rgba(255,_255,_255,_1)_100%)]">
         <div className="flex flex-col items-center justify-center text-center p-4">
-          <h1 className="text-4xl font-bold mb-4">
+          <h1 className="text-[25px] md:text-4xl font-bold mb-4">
             Oops! Página Não Encontrada
           </h1>
           <p className="text-lg text-muted-foreground mb-8">
